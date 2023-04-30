c6275b3f6559041310cbe8e473b39dd257f61faa
Kovas Palunas <kovas.palunas@gmail.com>
2023-04-30 14:09:29 -0700

    added release building and possible css fix

--- run.zsh
+++ run.zsh
@@ -7,9 +7,12 @@ jobs=()
 trap '((#jobs == 0)) || kill $jobs' EXIT HUP TERM INT
 
 # raspberrypi doesn't have enough memory to run both servers at once.
+# This is for development only anyway
 if [[ $(hostname) != *raspberrypi* ]]; then
   clj -M:frontend & jobs+=($!)
 fi
+# This is for actually running the server in non-dev mode.
+clj -M:frontend-release
 clj -M:api & jobs+=($!)
 
 wait
